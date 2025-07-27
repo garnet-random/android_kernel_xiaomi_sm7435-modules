439cf37ca21e15e47490a46518d6b1f711190ab6
AdarshGrewal <adarshgrewal@gmail.com>
2025-07-27 13:23:44 +0530

    display-kernel: dsi: Enable backlight on DPMS ON
    Change-Id: Ie96ba13db1e44dde09bbda0c684912fdcfe457f3

--- qcom/opensource/display-drivers/msm/dsi/dsi_display.c
+++ qcom/opensource/display-drivers/msm/dsi/dsi_display.c
@@ -1475,6 +1475,7 @@ int dsi_display_set_power(struct drm_connector *connector,
 		rc = dsi_panel_set_lp2(display->panel);
 		break;
 	case SDE_MODE_DPMS_ON:
+		display->panel->mi_cfg.bl_enable = true;
 		if ((display->panel->power_mode == SDE_MODE_DPMS_LP1) ||
 			(display->panel->power_mode == SDE_MODE_DPMS_LP2)) {
 			rc = dsi_panel_set_nolp(display->panel);
